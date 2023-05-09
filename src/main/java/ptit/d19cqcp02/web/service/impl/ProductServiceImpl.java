2e0945236fdbf5e771fa6da611d40d9103af55f6
My <n19dccn112@student.ptithcm.edu.vn>
2023-05-09 22:42:49 +0700

    thêm event

--- src/main/java/ptit/d19cqcp02/web/service/impl/ProductServiceImpl.java
+++ src/main/java/ptit/d19cqcp02/web/service/impl/ProductServiceImpl.java
@@ -94,8 +94,11 @@ public class ProductServiceImpl implements IBaseService<ProductDTO, Long>, IMode
         ProductDTO dto = modelMapper.map(entity, ProductDTO.class);
         dto.setCategoryName(entity.getCategory().getCategoryName());
         dto.setCateId(entity.getCategory().getCateId());
-        if (entity.getEvent().getEventId() != null)
+        try {
             dto.setEventId(entity.getEvent().getEventId());
+        } catch (Exception e) {
+
+        }
         if (entity.getFeatures() != null || entity.getFeatures().size() != 0) {
             dto.setFeaturesName(entity.getFeatures().stream().map(Feature::getFeatureSpecific).collect(Collectors.joining(" ")));
             dto.setFeatureIds(entity.getFeatures().stream().map(Feature::getFeatureFeatureId).collect(Collectors.toSet()));
